4872959fa8072f9078bda7434b6e63cf130462a0
dth <info@dthlabs.com>
2023-07-26 11:46:05 -0400

    updating events controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsController } from './events.controller';
+import { CatsController } from './cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, EventsController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
