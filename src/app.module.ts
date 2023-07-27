28c8b51aab0bf9860b60d52cacda8769dd75b3fe
dth <info@dthlabs.com>
2023-07-27 12:44:47 -0400

    CreateEventDTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
-import { CatsController } from './cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, EventsController, CatsController],
+  controllers: [AppController, EventsController],
   providers: [AppService],
 })
 export class AppModule {}
